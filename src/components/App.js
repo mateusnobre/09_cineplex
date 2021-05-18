4fe47e10c4133aa798f4e59dd1968a5d6dd25792
mateusnobre <mateusnobrests@gmail.com>
2021-05-18 12:34:41 -0300

    Initial project setup and Top component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Top from './Top';
+import MovieFeed from './MovieFeed';
+import React from 'react'
+
+export default function App() {
+    return(
+        <body>
+                <Top />
+        </body>
+    );
+}
---
+++ src/components/Top.js
@@ -0,0 +1,7 @@
+export default function Top(){
+    return(
+        <div class='top'>
+            CINEFLEX
+        </div>
+    )
+}
