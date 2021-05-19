6fbda83f4e966dc5ddcd35e771806ec39b6e01f8
mateusnobre <mateusnobrests@gmail.com>
2021-05-19 04:49:34 -0300

    fix typo

--- src/components/Showtimes.js
+++ src/components/Showtimes.js
@@ -8,7 +8,7 @@ export default function Showtimes(props){
                     <div class = 'showtimes'>
                         {session.showtimes.map(showtime =>
                             <Link to={'/assentos/' + (showtime.id)} style={{ textDecoration: 'none' }}>
-                                <div class='showtime' key={showtime.id} onClick={() => props.setShowtime(session.date + showtime.name)}>
+                                <div class='showtime' key={showtime.id} onClick={() => props.setShowtime(session.date + ' ' +showtime.name)}>
                                     {showtime.name}
                                 </div>
                             </Link>
